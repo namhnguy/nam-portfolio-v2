4da34dab0ed0ff8f811f28beefac894ec1ed15d7
Nam <namhnguy@gmail.com>
2022-07-23 16:08:15 -0500

    add in skills

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/navBar/NavBar';
 import Banner from './components/banner/Banner';
+import Skills from './components/skills/Skills';
 
 function App() {
   return (
     <div className='App'>
       <NavBar />
       <Banner />
+      <Skills />
     </div>
   );
 }
