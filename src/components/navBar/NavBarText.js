0446a52394d079b39362e07fd5779c08d9f40e94
Nam <namhnguy@gmail.com>
2022-07-23 12:32:21 -0500

    finished navigation

---
+++ src/components/navBar/NavBarText.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import navIcon1 from '../../assets/img/nav-icon1.svg';
+import navIcon2 from '../../assets/img/nav-icon2.svg';
+import navIcon3 from '../../assets/img/nav-icon3.svg';
+
+const NavBarText = () => {
+  return (
+    <span className='navbar-text'>
+      <div className='social-icon'>
+        <a href='#'>
+          <img src={navIcon1} alt='' />
+        </a>
+        <a href='#'>
+          <img src={navIcon2} alt='' />
+        </a>
+        <a href='#'>
+          <img src={navIcon3} alt='' />
+        </a>
+      </div>
+      <button className='vvd' onClick={() => console.log('connect')}>
+        <span>Let's Connect</span>
+      </button>
+    </span>
+  );
+};
+
+export default NavBarText;
